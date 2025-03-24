1cb1815f2c9f09d516938494ec70cf2985930eab
sabinakulbaeva <sabinakulbaeva1@mail.ru>
2025-03-24 15:37:18 +0300

    create

--- kulbaeva_repository-LR3.cpp
+++ kulbaeva_repository-LR3.cpp
@@ -1,16 +1,84 @@
 #include <iostream>
-#include <vector>
-#include <string>
-
 using namespace std;
 
-int main()
-{
-    vector<string> msg {"Hello", "C++", "World", "from", "VS Code", "and the C++ extension!"};
+// Прототипы функций
+void inputX();
+void inputY();
+void inputZ();
+double sumFractionalParts(double x, double y, double z);
+int sumIntegerParts(int x, int y, int z);
 
-    for (const string& word : msg)
-    {
-        cout << word << " ";
+int main() {
+    int choice = 0;
+    
+    while(true) {
+        // Вывод меню
+        cout << "Меню:" << endl;
+        cout << "1. Ввести вещественное число X" << endl;
+        cout << "2. Ввести вещественное число Y" << endl;
+        cout << "3. Ввести вещественное число Z" << endl;
+        cout << "4. Найти сумму дробных частей трех чисел" << endl;
+        cout << "5. Найти сумму целых частей трех чисел" << endl;
+        cout << "6. Выход" << endl;
+        
+        cout << "Ваш выбор: ";
+        cin >> choice;
+
+        switch(choice) {
+            case 1:
+                inputX();  // Функция ввода числа X
+                break;
+            case 2:
+                inputY();  // Функция ввода числа Y
+                break;
+            case 3:
+                inputZ();  // Функция ввода числа Z
+                break;
+            case 4:
+                // Вызываем функцию суммы дробных частей
+                cout << "Сумма дробных частей: " << sumFractionalParts(0, 0, 0) << endl;
+                break;
+            case 5:
+                // Вызываем функцию суммы целых частей
+                cout << "Сумма целых частей: " << sumIntegerParts(0, 0, 0) << endl;
+                break;
+            case 6:
+                return 0;  // Завершение программы
+            default:
+                cout << "Неверный ввод! Попробуйте снова." << endl;
+        }
     }
-    cout << endl;
+}
+
+// Реализация функций
+
+void inputX() {
+    // Здесь реализуйте ввод вещественного числа X
+    cout<<"Функция не реализована"<<endl;
+    // Ответственный Олег- ветка branch-fun_1
+}
+void inputY() {
+    // Здесь реализуйте ввод вещественного числа Y
+    //Ответственный Александр -ветка branch_fun_2
+    cout << "Функция не реализована." << endl;
+}
+
+void inputZ() {
+    // Здесь реализуйте ввод вещественного числа Z
+    // Ответственная Евгения- ветка branch_fun_3
+    cout << "Функция не реализована." << endl;
+}
+
+double sumFractionalParts(double x, double y, double z) {
+    // Здесь реализуйте вычисление суммы дробных частей трех чисел
+   //Ответственная Ольга -ветка branch_fun_4
+   cout << "Функция не реализована." << endl;
+    return 0.0;  // Пустое значение, замените на результат
+}
+
+int sumIntegerParts(int x, int y, int z) {
+    // Здесь реализуйте вычисление суммы целых частей трех чисел
+   // Ответственная Мария, ветка branch_fun_5
+   cout << "Функция не реализована." << endl;
+    return 0;  // Пустое значение, замените на результат
 }
